b461e42f099fcb8abded11ade4dd2085907ab11c
violet chang <violet7chang@gmail.com>
2019-07-31 19:05:42 -0500

    updated burger.js

--- models/burger.js
+++ models/burger.js
@@ -9,6 +9,51 @@ $(function(){
         };
 
         //send the PUT request
-        $.ajax("api/burgers/"+id,{})
-    })
+        $.ajax("api/burgers/"+id,{
+            type: "PUT",
+            data: newChompState
+        }).then(
+            function(){
+                console.log("changed chomp state to", newChomp);
+                //reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event){
+        event.preventDefault();
+
+        var newBurger={
+            name: $("#burger").val().trim(),
+            chomp: $("[name=chomp]:checked").val().trim()
+        };
+
+        //sent the POST request
+        $.ajax("/api/burgers",{
+            type: "POST",
+            data: newBurger
+        }).then(
+            function(){
+                console.log("created new burger", id);
+                //reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+$(".delete-burger").on("click", function(event){
+    var id=$(this).data("id");
+
+    //send the  DELETE request.
+    $.ajax("/api/burgers/" + id,{
+type: "DELETE"
+    }).then(
+        function(){
+            console.log("deleted burger", id);
+            //reload the page to get the updated list
+            location.reload();
+        }
+    )
+})
+
 })
