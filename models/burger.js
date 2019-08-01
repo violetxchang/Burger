3b6a9751a6362269ec4dd642dd95fb8b9c1d1b8a
violet chang <violet7chang@gmail.com>
2019-07-31 22:45:57 -0500

    burger.js update

--- models/burger.js
+++ models/burger.js
@@ -1,59 +1,22 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function(){
-    $(".change-chomp").on("click", function(event){
-        var id=$(this).data("id");
-        var newChomp=$(this).data("newdevoured");
+var orm = require("../config/orm.js");
 
-        var newChompState={
-            Chomp: newChomp
-        };
+var burger={
+    all: function(cb){
+        orm.all("burgers", function(res){
+            cb(res);
+        });
+    },
+    //the variables cols and vals are arrays
+    create: function(cols, vals, cb){
+        orm.create("burgers",cols, vals, function(res){
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb){
+        orm.update("burgers", objColVals, condition, function(res){
+            cb(res);
+        });
+    },
+};
 
-        //send the PUT request
-        $.ajax("api/burgers/"+id,{
-            type: "PUT",
-            data: newChompState
-        }).then(
-            function(){
-                console.log("changed chomp state to", newChomp);
-                //reload the page to get the updated list
-                location.reload();
-            }
-        );
-    });
-
-    $(".create-form").on("submit", function(event){
-        event.preventDefault();
-
-        var newBurger={
-            name: $("#burger").val().trim(),
-            chomp: $("[name=chomp]:checked").val().trim()
-        };
-
-        //sent the POST request
-        $.ajax("/api/burgers",{
-            type: "POST",
-            data: newBurger
-        }).then(
-            function(){
-                console.log("created new burger", id);
-                //reload the page to get the updated list
-                location.reload();
-            }
-        );
-    });
-$(".delete-burger").on("click", function(event){
-    var id=$(this).data("id");
-
-    //send the  DELETE request.
-    $.ajax("/api/burgers/" + id,{
-type: "DELETE"
-    }).then(
-        function(){
-            console.log("deleted burger", id);
-            //reload the page to get the updated list
-            location.reload();
-        }
-    )
-})
-
-})
+module.exports=burger;
