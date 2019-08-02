02b60687210fd7e8efd2bafa347a551f0e474931
violet chang <violet7chang@gmail.com>
2019-08-01 21:14:08 -0500

    db update

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
 	id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(250) not null,
-	devoured BOOLEAN DEFAULT false,
+    burger_name varchar(150) not null,
+	chomped BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
