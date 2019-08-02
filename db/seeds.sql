90301fa33bfeab7752909d6b0a8b047d9daa538d
violet chang <violet7chang@gmail.com>
2019-08-01 21:45:54 -0500

    finishing touches

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
 INSERT INTO burgers (burger_name) VALUES ('Red Castle Sliders');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, chomped) VALUES ('Veggie Burger', true);
