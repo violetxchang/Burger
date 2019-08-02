31c159416f2480432ac550f49c71a3bde8208d6a
violet chang <violet7chang@gmail.com>
2019-08-01 21:03:19 -0500

    udpated burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,20 @@
 var orm = require("../config/orm.js");
 
 var burger={
-    all: function(cb){
+    selectAll: function(cb){
         orm.all("burgers", function(res){
             cb(res);
         });
     },
-    //the variables cols and vals are arrays
-    create: function(cols, vals, cb){
-        orm.create("burgers",cols, vals, function(res){
-            cb(res);
-        });
+    insertOne: function(cols, vals, cb){
+        orm.create("burgers", cols, vals, cb);
     },
-    update: function(objColVals, condition, cb){
-        orm.update("burgers", objColVals, condition, function(res){
-            cb(res);
-        });
+    updateOne: function(objColVals, condition, cb){
+        orm.update("burgers", objColVals, condition, cb);
     },
+    deleteOne: function(condition, cb){
+        orm.delete("burgers", condition, cb);
+    }
 };
 
 module.exports=burger;
