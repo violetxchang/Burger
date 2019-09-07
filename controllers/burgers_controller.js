23791cba43be655b94b5b3d73d29da6eea8cf819
violet chang <violet7chang@gmail.com>
2019-09-06 22:23:28 -0500

    all cleaned up

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -44,7 +44,7 @@ router.put("/api/burgers/:id", function(req,res){
     });
 });
 
-router.delete("/api/cats/:id", function(req,res){
+router.delete("/api/burgers/:id", function(req,res){
     var condition="id = " + req.params.id;
 
     burger.deleteOne(condition, function(result){
